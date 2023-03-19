6aff58d8ea8a8bba588311b107ecaff7b2910415
brian <braintm2@gmail.com>
2023-03-19 16:01:27 -0700

    stripe

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ router.use('/admin', adminRouter);
 
 // This is your test secret API key.
 const stripe = require('stripe')('sk_test_51MnTRwC3qhij2vZlCUNW9BmfKG2Uop8Lu2c9ov17mxxBf5EW4O1mvd9uKrlzW5CJo42ooGzIq2d5cyYlaG1pTbz8008PtPRdF3');
-const YOUR_DOMAIN = 'https://puppy-paradise-api.onrender.com';
+const YOUR_DOMAIN = 'http://localhost:3000';
 
 router.post('/create-checkout-session', async (req, res) => {
     const { cartItems } = req.body;
