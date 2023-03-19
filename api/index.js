f25c3909db1a3784051f502b00570de5360b38ab
brian <braintm2@gmail.com>
2023-03-19 14:51:42 -0700

    more stripe tests

--- api/index.js
+++ api/index.js
@@ -30,6 +30,41 @@ router.use('/orders', ordersRouter);
 const adminRouter = require('./admin');
 router.use('/admin', adminRouter);
 
+// This is your test secret API key.
+const stripe = require('stripe')('sk_test_51MnTRwC3qhij2vZlCUNW9BmfKG2Uop8Lu2c9ov17mxxBf5EW4O1mvd9uKrlzW5CJo42ooGzIq2d5cyYlaG1pTbz8008PtPRdF3');
+app.use(express.static('public'));
+
+const YOUR_DOMAIN = 'https://puppy-paradise-api.onrender.com';
+
+app.post('/create-checkout-session', async (req, res) => {
+    const { cartItems } = req.body;
+
+    const line_items = cartItems.map(item => {
+        const line_item = {
+            price_data: {
+                currency: 'usd',
+                product_data: {
+                    name: item.name,
+                    description: item.breed,
+                    images: item.image1
+                },
+                unit_amount_decimal: Number(item.price)
+            },
+            quantity: 1,
+        }
+
+        return line_item;
+    }) 
+    const session = await stripe.checkout.sessions.create({
+        line_items,
+        mode: 'payment',
+        success_url: `${YOUR_DOMAIN}?success=true`,
+        cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+    });
+
+    res.redirect(303, session.url);
+});
+
 // error handling middleware
 router.use((error, req, res, next) => {
     res.send({
--- app.js
+++ app.js
@@ -10,42 +10,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 const apiRouter = require('./api');
-const { addItemToCart } = require("./db");
 app.use('/api', apiRouter);
-
-// This is your test secret API key.
-const stripe = require('stripe')('sk_test_51MnTRwC3qhij2vZlCUNW9BmfKG2Uop8Lu2c9ov17mxxBf5EW4O1mvd9uKrlzW5CJo42ooGzIq2d5cyYlaG1pTbz8008PtPRdF3');
-app.use(express.static('public'));
-
-const YOUR_DOMAIN = 'https://puppy-paradise-api.onrender.com';
-
-app.post('/create-checkout-session', async (req, res) => {
-    const { cartItems } = req.body;
-
-    const line_items = cartItems.map(item => {
-        const line_item = {
-            price_data: {
-                currency: 'usd',
-                product_data: {
-                    name: item.name,
-                    description: item.breed,
-                    images: item.image1
-                },
-                unit_amount_decimal: Number(item.price)
-            },
-            quantity: 1,
-        }
-
-        return line_item;
-    }) 
-    const session = await stripe.checkout.sessions.create({
-        line_items,
-        mode: 'payment',
-        success_url: `${YOUR_DOMAIN}?success=true`,
-        cancel_url: `${YOUR_DOMAIN}?canceled=true`,
-    });
-
-    res.redirect(303, session.url);
-});
 
 module.exports = app;
